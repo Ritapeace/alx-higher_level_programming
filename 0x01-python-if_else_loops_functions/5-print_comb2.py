2550590caabd6bbf17d07afccf022878ca206a6a
Ritapeace <99409347+Ritapeace@users.noreply.github.com>
2022-05-31 20:08:11 -0700

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print(', '.join(['{:02d},'.format(n)for n in range(99)]))
+for num in range(0, 100):
+        if num != 99:
+                print("{:02d}, ".format(num), end='')
+        else:
+                print("{:02d}".format(num))
