df8f6c00bc807a942108198993f6d3127756aa78
Ritapeace <ritapeace1997@gmail.com>
2022-09-26 07:10:46 -0700

    Script that takes in a URL and an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""takes in a URL and an email, sends a POST request to the
-passed URL with the email as a parameter, and displays the
- body of the response (decoded in utf-8)"""
+"""
+A Python script that takes in a URL and an email
+sends a POST request to the passed URL with the
+email as a parameter, and displays the body of
+the response (decoded in utf-8)
+"""
+
+from urllib import request
+from urllib import parse
+from sys import argv
 
 
-if __name__ == "__main__":
-    import urllib.request
-    import urllib.parse
-    import sys
+def main():
+    """main function"""
 
-    value = {'hr@holbertonschool.com
-': sys.argv[2]}
-    data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8", "replace"))
+    data = {'email': argv[2]}
+    data = parse.urlencode(data).encode()
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as resp:
+        content = resp.read().decode('utf-8')
+        print(content)
+
+
+if __name__ == '__main__':
+    main()
